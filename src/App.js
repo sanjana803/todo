ccccf0ee232cada6c8312ed827bfaf35523bceff
sanjana803 <kumarisanjana.cer18@itbhu.ac.in>
2021-04-29 02:40:25 +0530

    First commit

---
+++ src/App.js
@@ -0,0 +1,13 @@
+import React from 'react';
+import './App.css';
+import Todol from './components/Todol';
+
+function App() {
+  return (
+    <div className='todo-app'>
+      <Todol />
+    </div>
+  );
+}
+
+export default App;
